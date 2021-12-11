c5d94b76c02f31ab5e06ac578e73d4fba64930db
Amani Touihri <44845156+amenytouihri@users.noreply.github.com>
2021-12-11 21:11:13 +0100

    .gitignore fix

---
+++ data/seeds/001_first-seed.js
@@ -0,0 +1,13 @@
+const profiles = [ 
+  { name: 'Imen' }, 
+  { name: 'Aymen' }, 
+  { name: 'Feres' }, 
+  { name: 'Nadia' }, 
+  { name: 'Mohamed' } 
+];
+exports.seed = function (knex) { 
+  return knex('profiles').del() .then(() => { 
+    return knex('profiles').insert(profiles) 
+  }) 
+};
+
